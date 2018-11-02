138f468356cf3ca3525c0315d06ab7e5146c0cab
William Lopera <lopera.william@gmail.com>
2018-11-02 00:17:01 -0500

    Crear vista angularjs-1

--- src/main/java/com/wlopera/cliente/cmf/rest/ChampionController.java
+++ src/main/java/com/wlopera/cliente/cmf/rest/ChampionController.java
@@ -1,11 +1,17 @@
 package com.wlopera.cliente.cmf.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.wlopera.api.ChampionDTO;
 import com.wlopera.api.SoccerWorldChampionshipApi;
 
 @Controller
@@ -21,11 +27,12 @@ public class ChampionController {
 	}
 
 	/**
-	  * Go to home.
-	  * 
-	  * Probar servicio rest activo => netstat -ano | find "8585"
-	  * @return
-	  */
+	 * Go to home.
+	 * 
+	 * Probar servicio rest activo => netstat -ano | find "8585"
+	 * 
+	 * @return
+	 */
 	// http://localhost:8585/
 	@RequestMapping("/")
 	public String home() {
@@ -34,20 +41,20 @@ public class ChampionController {
 	}
 
 	/**
-	  * Go to champion.
-	  * 
-	  * @return
-	  */
+	 * Go to champion.
+	 * 
+	 * @return
+	 */
 	@RequestMapping("/champion")
 	public String goHome() {
 		return "champion";
 	}
 
 	/**
-	  * Servicio Rest listado de todos los campeonatos
-	  * 
-	  * @return Listado de campeonatos
-	  */
+	 * Servicio Rest listado de todos los campeonatos
+	 * 
+	 * @return Listado de campeonatos
+	 */
 	// http://localhost:8585/champion/allChampions
 	@RequestMapping(value = "/champion/allChampions")
 	@ResponseBody
@@ -57,9 +64,50 @@ public class ChampionController {
 		ChampionWrapper clientWrapper = new ChampionWrapper();
 
 		clientWrapper.setChampions(swcsApi.getListChampionsWin());
-		System.out.println("Campeones: " + clientWrapper.getChampions().toString());
+		System.out.println("getAllChampions: " + clientWrapper.getChampions().toString());
 		return clientWrapper;
-	
+
 	}
 
+	/**
+	 * Servicio Rest campeon por annio
+	 * 
+	 * @return Campeon para un annio requerido
+	 */
+	// http://localhost:8585/champion/championByYear/1986
+	@GetMapping(value = "/champion/championByYear/{year}")
+	@ResponseBody
+	public ChampionWrapper getChampionByYear(@PathVariable("year") String year) {
+
+		System.out.println("Inicio consulta servicio rest getChampionByYear para el año: " + year);
+		ChampionWrapper clientWrapper = new ChampionWrapper();
+
+		List<ChampionDTO> champions = new ArrayList<>();
+		champions.add(swcsApi.getChampionByYear(year));
+
+		clientWrapper.setChampions(champions);
+		System.out.println("getChampionByYear: " + clientWrapper.getChampions().toString());
+		return clientWrapper;
+
+	}
+
+	/**
+	 * Servicio Rest listado de todos los campeones ganadores para una catidad
+	 * exacta de campeonatos
+	 * 
+	 * @return Listado de campeonatos
+	 */
+	// http://localhost:8585/champion/championsByWins/2
+	@GetMapping(value = "/champion/championsByWins/{numberWins}")
+	@ResponseBody
+	public ChampionWrapper getChampionsByWins(@PathVariable("numberWins") String numberWins) {
+
+		System.out.println("Inicio consulta servicio rest getChampionsByWins para partidos ganados: " + numberWins);
+		ChampionWrapper clientWrapper = new ChampionWrapper();
+
+		clientWrapper.setChampions(swcsApi.getListChampionsWinByQty(numberWins));
+		System.out.println("getChampionsByWins: " + clientWrapper.getChampions().toString());
+		return clientWrapper;
+
+	}
 }
